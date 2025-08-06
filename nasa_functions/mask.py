c65c87b48a1c3fbbe760f5f3d297ea992b794a4f
Fernando Fernandes <fernando.costa@fisica.ufmt.br>
2025-08-06 16:08:49 -0300

    update classe Maiac

--- nasa_functions/mask.py
+++ nasa_functions/mask.py
@@ -111,159 +111,185 @@ def calcular_aod_550(df, colunas_aod, comprimentos_onda_nm):
     df['440-675_Angstrom_Exponent'] = df['440-675_Angstrom_Exponent'].replace(-999.0,np.nan)
     df_new = df[['time','AOD_550nm_est','440-675_Angstrom_Exponent']]
     return df_new.dropna()
+import rioxarray as rxr
+import xarray as xr
+import numpy as np
+from datetime import datetime, timedelta
+from pyproj import CRS, Transformer
+import os
+import pandas as pd
+
 
 class Maiac:
-    def __init__(self,path,var,scale_factor,fillValue,index_hdf_aod,index_hdf_angle,mask,coordinates:tuple[float,float],window_size:int,number_int:int):
-        self.path = path
-        self.var = var
-        self.index_hdf_aod = index_hdf_aod
-        self.index_hdf_angle = index_hdf_angle
-        self.scale_factor = scale_factor 
-        self.fillValue = fillValue
-        self.coordinates = coordinates
-        self.mask = mask
-        self.window_size = window_size
-        self.number_int = number_int
-
+    def __init__(self, path_HDF : str,
+                 var_ds: str,
+                 hdf_index: str,
+                 coordinate_time: str,
+                 dict_stations: dict,
+                 window_sizes: list[int],
+                 year: str,
+                 index_iteraction:str,
+                 product_name: str,
+                 _FillValue:int ,
+                 scale_factor:int):
+        self.path_HDF = path_HDF
+        self.var_ds = var_ds
+        self.hdf_index = hdf_index
+        self.coordinate_time = coordinate_time
+        self.dict_stations = dict_stations 
+        self.window_sizes = window_sizes
+        self.year = year
+        self.index_iteraction = index_iteraction 
+        self.product_name = product_name 
+        self._FillValue = _FillValue
+        self.scale_factor = scale_factor
 
     @staticmethod
-    def extract_time(data_list:str):
-        year,julian_day,hour,minute = int(data_list[:4]), int(data_list[4:7]), int(data_list[7:9]), int(data_list[9:])
-        date = datetime(year, 1, 1) + timedelta(days=julian_day - 1)
-        month,day = date.month, date.day
-        string_year = f'{year}-{month}-{day} {hour}:{minute}:00'
-        dt = datetime.strptime(string_year,"%Y-%m-%d %H:%M:%S") 
-        return str(dt)
-    
-    @staticmethod
-    def apply_mask(qa_array, mask_name, value_names):
-        """
-        Applying multiple maks.
-        """
-        mask_info = BITMASKS[mask_name]
-        offset = mask_info["bit_offset"]
-        length = mask_info["bit_length"]
-
-        if not isinstance(value_names, list):
-            value_names = [value_names]
-
-        mask = xr.zeros_like(qa_array, dtype=bool)
-        for value_name in value_names:
-            value = mask_info["values"][value_name]
-            condition = ((qa_array >> offset) & ((1 << length) - 1)) == value
-            mask |= condition
-        return mask
-    
-    @staticmethod
-    def sinusoidal_to_latlon(coor: tuple[float, float]):
-        """
-        convert geographic coordinate (degrees) (lat,lon)
-        in sinusoidal coordinates (meters) ----> returning (lon,lat)
-        """
-        crs_sinu = CRS.from_wkt(wkt)
-        crs_geo = CRS.from_proj4("+proj=longlat +datum=WGS84 +no_defs")
-        transformer = Transformer.from_crs(crs_geo, crs_sinu, always_xy=True)
-        lon, lat = coor[1], coor[0] 
-        x, y = transformer.transform(lon, lat)
-        return x, y # -----> (lon, lat)
-    
-    @staticmethod
-    def select_pixels(ds,station: tuple[float, float],window_size):
-        lon,lat = Maiac.sinusoidal_to_latlon((station[0],station[1]))
+    def select_pixels(ds,
+                    station: tuple[float, float],
+                    window_size:int):
+        lat,lon = station[0],station[1]
         lat_idx = np.abs(ds.lat.values - lat).argmin()
         lon_idx = np.abs(ds.lon.values - lon).argmin()
         lat_start = max(0, lat_idx - window_size)
         lat_end = min(len(ds.lat), lat_idx + window_size + 1)  
         lon_start = max(0, lon_idx - window_size)
         lon_end = min(len(ds.lon), lon_idx + window_size + 1)
-
         DatArray = ds.isel(
-                lat=slice(lat_start, lat_end),
-                lon=slice(lon_start, lon_end))
+                        lat=slice(lat_start, lat_end),
+                        lon=slice(lon_start, lon_end))
         return DatArray
-    
-    def applying_mask(self):
-        ds = rxr.open_rasterio(self.path)[self.index_hdf_aod]
-        qa = ds['AOD_QA']
-        mask_ideal = xr.ones_like(qa, dtype=bool)
-        for mask_name, value_name in self.mask.items():
-            condition = Maiac.apply_mask(qa, mask_name, value_name)
-            mask_ideal &= condition
-        new_ds = ds.where(mask_ideal)
-        return new_ds
-    
-    
-    def filter_data(self):
-        """
-        Organizing your file, according to your filter
-        """   
-        ds = rxr.open_rasterio(self.path)[self.index_hdf_aod]
-        qa = ds['AOD_QA']
-        mask_ideal = xr.ones_like(qa, dtype=bool)
-
-        for mask_name, value_name in self.mask.items():
-            condition = Maiac.apply_mask(qa, mask_name, value_name)
-            mask_ideal &= condition
-        new_ds = ds.where(mask_ideal)
-        ds_new = new_ds.where(mask_ideal).rename({'x': 'lon', 'y': 'lat'})
-        ds_new = ds_new[self.var].where(ds_new[self.var] != self.fillValue) * self.scale_factor
-        lista = ds.attrs.get('Orbit_time_stamp').split(' ')
-        list_datas = [k[:-1] for k in lista if k]
-        lista_datas = [Maiac.extract_time(n) for n in list_datas]
-        ds_new = ds_new.assign_coords(band=("band", lista_datas))
-        ds_new = ds_new.rename({'band': 'time'})
-        return ds_new
     @staticmethod
-    def extract_csv_from_hdf(ds,
-                    coords: tuple[float,float],
-                    central_pixel: bool = True,
-                    window_size: int = 1):
-        '''
-        ds -> DataArray
-        coords -> tuple (lat,lon)
-        central_pixels -> get data around stations only if have data station
-        '''
-        ds = Maiac.select_pixels(ds,(coords[0],coords[1]),window_size)
-        central_value = ds.isel(lat=window_size, lon=window_size)
-        is_valid = central_value.notnull()
-            
-        # create a dataframe for each time
-        results = []
-        for i in range(len(ds.time)):
-            time_val = ds.time[i].values
-            current_valid = is_valid[i].item() if len(is_valid.shape) > 0 else is_valid.item()
-            result_dict = {
-                    'time': time_val,
-                    'aod_inst': np.nan,
-                    f'mean_aod_{window_size}x{window_size}_px': np.nan,
-                    'std_aod': np.nan,
-                    'samples': 0
-                }
-            if current_valid or not central_pixel:
-                current_ds = ds.isel(time=i)
-                valid_counts = current_ds.count().item()
-                result_dict.update({
-                        'aod_inst': float(central_value[i].item()),
-                        f'mean_aod_{window_size}x{window_size}_px': float(current_ds.mean().item()),
-                        'std_aod': float(current_ds.std().item()),
-                        'samples': int(valid_counts),
-                    })
-                
-            results.append(result_dict)
-            
-        return pd.DataFrame(results)
-    
-    def extract_csv(self):
+    def get_mean_and_STD(ds_filled,time_index):
         """
-        Organizing your file, according to your filter
-        """  
-        ds = self.filter_data()  # Corrigido: usando self em vez de maiac
-        data_frame = self.extract_csv_from_hdf(ds, self.coordinates, window_size = self.window_size)
-        year_file = self.path[8:-54]
-        os.makedirs(f'csv_file/{year_file}_{self.window_size}',exist_ok=True)
-        data_frame.to_csv(f'csv_file/{year_file}_{self.window_size}/{year_file}_{self.number_int}.csv')
-
-        return data_frame  # Adicionei um return para que o resultado possa ser armazenado
+        ds_filled -->> data spatial filtered around the station.
+        var -->> name of var of interesting
+        time_index -->> it's important, because you have diferents arrays in time,
+        you need to specify the index of time.
+        """
+        val_mean = ds_filled.isel(time=time_index).mean().values.item()
+        std_val = ds_filled.isel(time=time_index).std().values.item()
+        return val_mean,std_val
+    @staticmethod
+    def valid_windows(number):
+        """
+        number --->> win_size
+        if my window size is equal to 1, i need to check the pixels around
+        the station, for that, its only add plus 2 and take square, after that i have 
+        the number of values in matrix (win_size+2)**2 = 9 pixels with center value.
+        But we need only half of samples avaible, for that, we take minus one pixel (central_value) and divided by/2.
+        """
+        number += 2  
+        return int((number**2-1)/2)
+    @staticmethod
+    def filter_data(path_hdf,var,fillValue,scale_factor,index_hdf):
+        # open_file
+        ds = rxr.open_rasterio(path_hdf)[index_hdf]
+        # until masks
+        qa = ds['AOD_QA']
+        qa_aod = ((qa >> 8) & 0b1111) == 0b0000
+        cloud_clear = (qa & 0b00000111) == 0b001
+        no_glint = ((qa >> 12) & 0b1) == 0
+        on_land = ((qa >> 3) & 0b11) == 0b00
+        mask_ideal = qa_aod & cloud_clear & no_glint & on_land
+        # aplying the mask 
+        new_ds = ds[var].where(ds[var] != fillValue)*scale_factor
+        new_ds = new_ds.where(mask_ideal).rename({'x':'lon','y':'lat'})
+        # get time
+        lista = ds.attrs.get('Orbit_time_stamp').split(' ')
+        list_datas = [k[:-1] for k in lista if len(k) > 0]
+        @staticmethod
+        def extract_time(data_list:str):
+                year,julian_day,hour,minute = int(data_list[:4]), int(data_list[4:7]), int(data_list[7:9]), int(data_list[9:])
+                date = datetime(year, 1, 1) + timedelta(days=julian_day - 1)
+                month,day = date.month, date.day
+                string_year = f'{year}-{month}-{day} {hour}:{minute}:00' 
+                dt = datetime.strptime(string_year,"%Y-%m-%d %H:%M:%S") 
+                return str(dt)
+        lista_datas = [extract_time(n) for n in list_datas]
+        new_ds = new_ds.assign_coords(band=("band", lista_datas))
+        new_ds = new_ds.rename({'band':'time'})
+        return new_ds
+    @staticmethod
+    def lat_to_Sinusoidal(coor: tuple[float, float]):
+            """
+            Converte coordenadas geográficas (lat, lon) para projeção sinusoidal (y, x em metros)
+            """
+            wkt = """PROJCS["unnamed",GEOGCS["Unknown datum based upon the custom spheroid",
+            DATUM["Not specified (based on custom spheroid)",
+            SPHEROID["Custom spheroid",6371007.181,0]],
+            PRIMEM["Greenwich",0],
+            UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]]],
+            PROJECTION["Sinusoidal"],
+            PARAMETER["longitude_of_center",0],
+            PARAMETER["false_easting",0],
+            PARAMETER["false_northing",0],
+            UNIT["Meter",1],
+            AXIS["Easting",EAST],
+            AXIS["Northing",NORTH]]"""
+            
+            crs_sinu = CRS.from_wkt(wkt)
+            crs_geo = CRS.from_proj4("+proj=longlat +datum=WGS84 +no_defs")
+            transformer = Transformer.from_crs(crs_geo, crs_sinu, always_xy=True)
+            
+            lon, lat = coor[1], coor[0]
+            lon, lat = transformer.transform(lon, lat)
+            return lat,lon
+    
+    def Getting_dataframe_from_netCDF_sequential(self,classe):
+        """
+        path_hdf -> path of your hdf file
+        hdf_index -> index of your hdf_file
+        coordinate_time -> name of your time coordinate 
+        dict_stations -> dicionary of your stations
+        window_sizes -> number of sizes of pixels around station
+        year - > year for diferenciate the outpufiles
+        index_iteraction -> number of file for diferenciate the output files
+        """ 
+        ds = classe.filter_data(path_hdf=self.path_HDF,
+                         var=self.var_ds,
+                         fillValue=self._FillValue,
+                         scale_factor=self.scale_factor,
+                         index_hdf=self.hdf_index)
+        dict_stations = self.dict_stations
+        data_list,size_time = [], len(ds.time)
+        for station in self.dict_stations:
+            coords_station, station_name = classe.lat_to_Sinusoidal(dict_stations[station]), station # lat_to_Sinusoidal(dict_stations[station])
+            for i_time in range(size_time):
+                dictionary_station = {
+                    'station': station_name,
+                    f'{self.coordinate_time}': ds[f'{self.coordinate_time}'].values[i_time]
+                }
+                sequence_broken = False 
+                for win in sorted(self.window_sizes): 
+                    
+                    if sequence_broken:
+                        dictionary_station[f'mean_px_{win}x{win}'] = np.nan
+                        dictionary_station[f'std_px_{win}x{win}'] = np.nan
+                        continue
+                    
+                    ds_filled = classe.select_pixels(ds=ds,
+                                            station=coords_station,
+                                            window_size=win)
+                    
+                    arr = ds_filled.isel(time=i_time).values
+                    valid_pixels = int(np.sum(~np.isnan(arr)))
+                    valid_win = classe.valid_windows(win)
+                    dictionary_station[f'inst_aod'] = arr[win][win]
+            
+                    if isinstance(valid_win, int) and valid_pixels >= valid_win:
+                        val_mean, val_std = get_mean_and_STD(ds_filled, i_time)
+                        dictionary_station[f'mean_px_{win}x{win}'] = val_mean
+                        dictionary_station[f'std_px_{win}x{win}'] = val_std
+                    else:
+                        dictionary_station[f'mean_px_{win}x{win}'] = np.nan
+                        dictionary_station[f'std_px_{win}x{win}'] = np.nan
+                        sequence_broken = True 
+                data_list.append(dictionary_station)
+        
+        df = pd.DataFrame(data_list)
+        os.makedirs('Parquet_datas', exist_ok=True)     
+        df.to_parquet(f'Parquet_datas/{self.product_name}_{self.year}_{self.index_iteraction}.parquet',index=False)
 
     
 
